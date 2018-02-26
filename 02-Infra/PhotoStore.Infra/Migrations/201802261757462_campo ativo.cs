79169b62922884ed52336f8d19eaf4ed892362d7
Vitor Luiz Rubio <vitorrubio@gmail.com>
2018-02-26 15:07:42 -0300

    ajustados indices do evento e foto

--- 02-Infra/PhotoStore.Infra/Migrations/201802261757462_campo ativo.cs
+++ 02-Infra/PhotoStore.Infra/Migrations/201802261757462_campo ativo.cs
@@ -7,11 +7,10 @@ namespace PhotoStore.Infra.Migrations
     {
         public override void Up()
         {
-            DropForeignKey("dbo.Foto", "Evento_Id", "dbo.Evento");
+
             AddColumn("dbo.ArquivoFoto", "Ativo", c => c.Boolean(nullable: false));
             AddColumn("dbo.Foto", "Vitrine", c => c.Boolean(nullable: false));
             AddColumn("dbo.Foto", "Ativo", c => c.Boolean(nullable: false));
-            AddColumn("dbo.Foto", "Evento_Id", c => c.Int());
             AddColumn("dbo.Evento", "Vitrine", c => c.Boolean(nullable: false));
             AddColumn("dbo.Evento", "Ativo", c => c.Boolean(nullable: false));
             AddColumn("dbo.Evento", "Capa_Id", c => c.Int());
@@ -19,18 +18,14 @@ namespace PhotoStore.Infra.Migrations
             AddColumn("dbo.TipoProduto", "Ativo", c => c.Boolean(nullable: false));
             AddColumn("dbo.ItemDoPedido", "Ativo", c => c.Boolean(nullable: false));
             AddColumn("dbo.Pedido", "Ativo", c => c.Boolean(nullable: false));
-            CreateIndex("dbo.Foto", "Evento_Id");
             CreateIndex("dbo.Evento", "Capa_Id");
             AddForeignKey("dbo.Evento", "Capa_Id", "dbo.Foto", "Id");
-            AddForeignKey("dbo.Foto", "Evento_Id", "dbo.Evento", "Id");
         }
         
         public override void Down()
         {
-            DropForeignKey("dbo.Foto", "Evento_Id", "dbo.Evento");
             DropForeignKey("dbo.Evento", "Capa_Id", "dbo.Foto");
             DropIndex("dbo.Evento", new[] { "Capa_Id" });
-            DropIndex("dbo.Foto", new[] { "Evento_Id" });
             DropColumn("dbo.Pedido", "Ativo");
             DropColumn("dbo.ItemDoPedido", "Ativo");
             DropColumn("dbo.TipoProduto", "Ativo");
@@ -38,11 +33,10 @@ namespace PhotoStore.Infra.Migrations
             DropColumn("dbo.Evento", "Capa_Id");
             DropColumn("dbo.Evento", "Ativo");
             DropColumn("dbo.Evento", "Vitrine");
-            DropColumn("dbo.Foto", "Evento_Id");
             DropColumn("dbo.Foto", "Ativo");
             DropColumn("dbo.Foto", "Vitrine");
             DropColumn("dbo.ArquivoFoto", "Ativo");
-            AddForeignKey("dbo.Foto", "Evento_Id", "dbo.Evento", "Id");
+
         }
     }
 }
